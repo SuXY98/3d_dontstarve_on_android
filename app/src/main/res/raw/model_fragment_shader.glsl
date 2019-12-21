f5520285af680d1d51d63b3d3819bcf052bea927
凯文 胡 <lostkevin666@gmail.com>
2019-12-21 22:17:46 +0800

    fix bug, varying variable must can be interpolated, so varying bool is illegal.

--- app/src/main/res/raw/model_fragment_shader.glsl
+++ app/src/main/res/raw/model_fragment_shader.glsl
@@ -1,7 +1,7 @@
 precision mediump float;
 uniform sampler2D vTexture;
 
-//varying bool hTexture;
+uniform bool hasTexture;
 varying vec2 tCoord;
 varying vec4 vDiffuse;
 varying vec4 vAmbient;
@@ -12,9 +12,9 @@ varying  vec4 vColor;
 void main() {
     //vec4 finalColor=hasTexture?texture2D(vTexture,tCoord):vColor;
     vec4 finalColor = vColor;
-    //if(hTexture){
+    if(hasTexture){
         finalColor=texture2D(vTexture,tCoord);
-    //}
+    }
     gl_FragColor=finalColor*vAmbient+finalColor*vSpecular+ finalColor*vDiffuse;
 }
 
